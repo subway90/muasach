475dc18379b42c5c89a87575dcf08be191ea9280
subway90 <nguyenhieu3105@gmail.com>
2024-04-15 12:15:24 +0700

    form footer

--- view/user/footer.php
+++ view/user/footer.php
@@ -1,41 +1,39 @@
 <!-- Footer Start -->
-<div class="container-fluid footer bg-primary">
+<div class="container-fluid footer border-top border-4">
     <div class="container py-5">
         <div class="row g-5">
             <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.1s">
                 <a href="index.html" class="d-inline-block mb-3">
-                    <h1 class="text-white">Hairnic</h1>
+                    <h1 class="text-primary">muasach.net</h1>
                 </a>
-                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat non
-                    malesuada consequat, nibh erat tempus risus, vitae porttitor purus nisl vitae purus.</p>
+                <p class="mb-0">Chuyên cung cấp các loại sách tiểu thuyết, truyện tranh, sách giáo khoa, dụng cụ học tập,...</p>
             </div>
             <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
-                <h5 class="mb-4">Get In Touch</h5>
-                <p><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                <h5 class="mb-4">Liên hệ</h5>
+                <p><i class="fa fa-map-marker-alt me-3"></i>102/5/4 Tân Thới Nhất 01, HCM</p>
+                <p><i class="fa fa-phone-alt me-3"></i>0979 651 651</p>
+                <p><i class="fa fa-envelope me-3"></i>contact@muasach.net</p>
                 <div class="d-flex pt-2">
-                    <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-facebook-f"></i></a>
                     <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-instagram"></i></a>
-                    <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-twitter"></i></a>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.5s">
-                <h5 class="mb-4">Our Products</h5>
-                <a class="btn btn-link" href="">Hair Shining Shampoo</a>
-                <a class="btn btn-link" href="">Anti-dandruff Shampoo</a>
-                <a class="btn btn-link" href="">Anti Hair Fall Shampoo</a>
-                <a class="btn btn-link" href="">Hair Growing Shampoo</a>
-                <a class="btn btn-link" href="">Anti smell Shampoo</a>
+                <h5 class="mb-4">Thể loại</h5>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Mannga</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Văn học</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Ligt Novel</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Tiểu thuyết</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Truyện tranh</a>
             </div>
             <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.7s">
-                <h5 class="mb-4">Popular Link</h5>
-                <a class="btn btn-link" href="">About Us</a>
-                <a class="btn btn-link" href="">Contact Us</a>
-                <a class="btn btn-link" href="">Privacy Policy</a>
-                <a class="btn btn-link" href="">Terms & Condition</a>
-                <a class="btn btn-link" href="">Career</a>
+                <h5 class="mb-4">Sách</h5>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Sách kỹ thuật</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Sách giáo khoa</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Sách văn phòng</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Sách tham khảo</a>
+                <a class="btn btn-link" href="<?=ACT?>san-pham">Sách đặc biệt</a>
             </div>
         </div>
     </div>
@@ -43,17 +41,15 @@
         <div class="copyright">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-
-                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                    Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                    &copy; <a class="border-bottom" href="#">Công ty TNHH MTH MS</a>
+                    | Thiết kế bởi <a class="border-bottom" href="https://github.com/subway90">HGroup - SUBWAY90</a>
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <div class="footer-menu">
-                        <a href="">Home</a>
-                        <a href="">Cookies</a>
-                        <a href="">Help</a>
-                        <a href="">FAQs</a>
+                        <a href="#">Điều khoản</a>
+                        <a href="#">Hỗ trợ</a>
+                        <a href="#">Cookies</a>
+                        <a href="#">FAQs</a>
                     </div>
                 </div>
             </div>

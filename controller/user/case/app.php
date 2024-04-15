138309b2cc700288a951b32eb0f902caaf5cd161
subway90 <nguyenhieu3105@gmail.com>
2024-04-16 05:27:53 +0700

    hide URL mode light/dark

--- controller/user/case/app.php
+++ controller/user/case/app.php
@@ -2,4 +2,5 @@
 if(isset($arrayURL[1]) && !empty($arrayURL[1])){
     if($arrayURL[1] == 'mode-light') $_SESSION['background'] = '';
     if($arrayURL[1] == 'mode-dark')  $_SESSION['background'] = 'style="background: #050314; color:#ffffff; opacity: 0.8"';
+    header('Location:'.ACT.$arrayURL[0]);    
 }
